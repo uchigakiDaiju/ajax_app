53d376e73d37e5070edca9ce53371a02908910e8
uchigakiDaiju <uchigaki.daiju@gmail.com>
2020-10-24 00:41:53 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,17 @@ class PostsController < ApplicationController
     Post.create(content: params[:content])
     redirect_to action: :index 
   end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked  # checkedはカラムで、boolean型。この場合、trueで黒くなっている
+      post.update(checked: false) #checked: falseは、白くする
+    else
+      post.update(checked: true) #checked: trueは黒くする
+    end
+
+    item = Post.find(params[:id])
+    render json: {post: item}
+
+  end
 end
